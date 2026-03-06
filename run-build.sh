ae2e63bc2563a31aa4ecac205e66feb9daa697c6
sandreas <2050604+sandreas@users.noreply.github.com>
2026-03-06 06:36:41 +0100

    changed build script to use absolute path

--- run-build.sh
+++ run-build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
+CURRENT_DIR="$(realpath $(dirname $0))"
+#echo $CURRENT_DIR
+#exit
 cd buildroot && \
-  make BR2_EXTERNAL=../buildroot-external-licheervnano licheervnano_defconfig && \
+  make BR2_EXTERNAL=$CURRENT_DIR/buildroot-external-licheervnano licheervnano_defconfig && \
   make
